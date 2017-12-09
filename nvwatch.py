dc545c43821dbe71875759441d84afae2717b8b7
Matti J. Kärki <mjk@iki.fi>
2017-12-09 22:24:33 +0200

    Reformatting code.

--- nvwatch.py
+++ nvwatch.py
@@ -2,22 +2,30 @@ import subprocess
 import re
 import time
 
+NVSMI = r'C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe'
+
 def getNVdata():
     """
     Returns GPU statistics in a list:
-    Fan speed (%)
-    Power usage (W)
-    Max power (W)
-    Memory usage (MiB)
-    Max memory (MiB)
-    GPU usage (%)
+    [Fan speed (%),
+     Power usage (W),
+     Max power (W),
+     Memory usage (MiB),
+     Max memory (MiB),
+     GPU usage (%)]
     """
     retval = list()
 
-    result = subprocess.run(r'C:\Program Files\NVIDIA Corporation\NVSMI\nvidia-smi.exe', stdout=subprocess.PIPE)
+    result = subprocess.run(NVSMI, stdout=subprocess.PIPE)
     res = result.stdout.decode('iso-8859-1')
 
-    m = re.search(r'\|.+?([0-9]+)%.+?([0-9]+)C.+?([0-9]+)W.+?\/.+?([0-9]+)W.+?\|.+?([0-9]+).+?\/.+?([0-9]+).+?\|.+?([0-9]+)%', res)
+    m = re.search(r'\|.+?([0-9]+)'
+                  r'%.+?([0-9]+)C'
+                  r'.+?([0-9]+)W'
+                  r'.+?\/.+?([0-9]+)W'
+                  r'.+?\|.+?([0-9]+)'
+                  r'.+?\/.+?([0-9]+)'
+                  r'.+?\|.+?([0-9]+)%', res)
 
     for i in range(1, 8):
         retval.append(int(m.group(i)))
@@ -33,19 +41,27 @@ if __name__ == '__main__':
     try:
         while True:
             rslt = getNVdata()
-            if memmax < rslt[4]:
-                memmax = rslt[4]
-            if loadmax < rslt[6]:
-                loadmax = rslt[6]
-            if tempmax < rslt[1]:
-                tempmax = rslt[1]
-            if powermax < rslt[2]:
+            if memmax    < rslt[4]:
+                memmax   = rslt[4]
+            if loadmax   < rslt[6]:
+                loadmax  = rslt[6]
+            if tempmax   < rslt[1]:
+                tempmax  = rslt[1]
+            if powermax  < rslt[2]:
                 powermax = rslt[2]
-            if fanmax < rslt[0]:
-                fanmax = rslt[0]
+            if fanmax    < rslt[0]:
+                fanmax   = rslt[0]
             
-            print('Mem: {4:4d}MiB    Load: {6:3d}%    Temp: {1:2d}C    Power: {2:3d}W    Fan: {0:3d}%'.format(*rslt))
+            print('Mem: {4:4d}MiB'
+                  '    Load: {6:3d}%'
+                  '    Temp: {1:2d}C'
+                  '    Power: {2:3d}W'
+                  '    Fan: {0:3d}%'.format(*rslt))
             time.sleep(1)
     except KeyboardInterrupt:
         print('\nPeak values:')
-        print('Mem: {0:4d}MiB    Load: {1:3d}%    Temp: {2:2d}C    Power: {3:3d}W    Fan: {4:3d}%'.format(memmax, loadmax, tempmax, powermax, fanmax))
+        print('Mem: {0:4d}MiB'
+              '    Load: {1:3d}%'
+              '    Temp: {2:2d}C'
+              '    Power: {3:3d}W'
+              '    Fan: {4:3d}%'.format(memmax, loadmax, tempmax, powermax, fanmax))
